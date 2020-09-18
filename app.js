1d42e66318f72317a48172d9a70c8cbb99018681
eyalstolero7 <eyalstolero@campus.technion.ac.il>
2020-09-18 13:33:39 +0300

    updated files

--- app.js
+++ app.js
@@ -114,7 +114,7 @@ app.post("/add/:list", function (req, res) {
     res.redirect("/" + _.kebabCase(newListName));
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log("Server is running on port 3000.");
 });
 
